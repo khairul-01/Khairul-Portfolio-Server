9ba8993a200f94b6936ab072c04dfc83a5c8a768
Khairul Alam <khairul431743@gmail.com>
2024-02-19 19:58:11 +0600

    added environment variable

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
+// DB_USER=khairul-portfolio
+// DB_PASS=M7ttgDH4WmGDIOZg
+
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
 
 app.get('/', (req, res) => {
     res.send('Khairul Porfolio server side is running');
