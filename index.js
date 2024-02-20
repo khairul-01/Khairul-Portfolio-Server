b079ca6119c1091f94eccdda6d7e9be0ae0b1fd2
Khairul Alam <khairul431743@gmail.com>
2024-02-20 17:04:34 +0600

    added skills and projects api

--- index.js
+++ index.js
@@ -13,10 +13,51 @@ app.use(cors());
 app.use(express.json());
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.52ba6.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+
+        const skillsCollection = client.db('KhairulPortfolio').collection('skills');
+        const projectsCollection = client.db('KhairulPortfolio').collection('projects');
+
+        app.get('/skills', async (req, res) => {
+            const result = await skillsCollection.find().toArray();
+            // console.log(result[0]);
+            res.send(result);
+        })
+
+        app.get('/projects', async (req, res) => {
+            const result = await projectsCollection.find().toArray();
+            res.send(result);
+        })
+
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Khairul Porfolio server side is running');
-} )
+})
 
 app.listen(port, () => {
     console.log(`Khairul Portfolio is running on port ${port}`);
